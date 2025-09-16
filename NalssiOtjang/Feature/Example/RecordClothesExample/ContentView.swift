7d81188edc15a49da91bfe45f531629b802f851b
jihun <jihun32@naver.com>
2025-09-16 20:49:19 +0900

    chore: 파일 정리 및 RecordClohtesExample 추가

---
+++ NalssiOtjang/Feature/Example/RecordClothesExample/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  RecordClothesExample
+//
+//  Created by home on 9/16/25.
+//
+
+import SwiftUI
+import RecordClothes
+
+struct ContentView: View {
+    
+    var body: some View {
+        VStack {
+            RecordClothesExpalinView()
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NalssiOtjang/Feature/Example/RecordClothesExample/RecordClothesExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecordClothesExampleApp.swift
+//  RecordClothesExample
+//
+//  Created by home on 9/16/25.
+//
+
+import SwiftUI
+
+@main
+struct RecordClothesExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
