f6f559c691fb83ce4c1819dbfc82e0b22917f616
jihun <jihun332@gmai.com>
2025-10-17 10:51:24 +0900

    chore: ImageClassifierMnager, Clothes  정리

--- NalssiOtjang/Core/MLImageClassification/Sources/MLImageClassifier/Manager/MLImageClassifierManager.swift
+++ NalssiOtjang/Core/MLImageClassification/Sources/MLImageClassifier/Manager/MLImageClassifierManager.swift
@@ -16,21 +16,24 @@ public struct MLImageClassifierManager: ClothesClassifier {
     public init() { }
     
     public func classify(imageData: Data) throws -> Clothes {
-        guard let coreMLModel = try? ClothesClassifierModel(configuration: MLModelConfiguration()),
-              let visionModel = try? VNCoreMLModel(for: coreMLModel.model) else {
-            fatalError("Loading CoreML Model Failed")
+        do {
+            var categories: [String] = []
+            
+            let coreMLModel = try ClothesClassifierModel(configuration: MLModelConfiguration())
+            let visionModel = try VNCoreMLModel(for: coreMLModel.model)
+            let request = VNCoreMLRequest(model: visionModel)
+            let handler = VNImageRequestHandler(ciImage: CIImage(data: imageData)!)
+            try handler.perform([request])
+            
+            if let observations = request.results as? [VNClassificationObservation] {
+                categories = observations
+                    .sorted(by: { $0.confidence > $1.confidence })
+                    .map { $0.identifier }
+            }
+            
+            return Clothes(categories: categories)
+        } catch {
+            throw error
         }
-        
-        let request = VNCoreMLRequest(model: visionModel)
-        let handler = VNImageRequestHandler(ciImage: CIImage(data: imageData)!)
-        try handler.perform([request])
-        var categories: [String] = []
-        if let observations = request.results as? [VNClassificationObservation] {
-            categories = observations
-                .sorted(by: { $0.confidence > $1.confidence})
-                .map { $0.identifier }
-        }
-        
-        return Clothes(categories: categories)
     }
 }
