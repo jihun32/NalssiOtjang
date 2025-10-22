c7e5357b50c48cc0b816b52eef86c84e951d032c
jihun <jihun332@gmai.com>
2025-10-22 20:06:14 +0900

    feat: 카테고리 koreanName 정리 - #20

--- NalssiOtjang/Domain/Clothes/Interface/Entity/Clothes.swift
+++ NalssiOtjang/Domain/Clothes/Interface/Entity/Clothes.swift
@@ -26,7 +26,6 @@ public struct Clothes {
             }
         }
     }
-    
 }
 
 // MARK: - Clothes Category
@@ -77,6 +76,75 @@ extension Clothes {
             case miniDress = "mini_dress"
             case mediumDress = "medium_dress"
             case longDress = "long_dress"
+        }
+    }
+}
+
+
+// MARK: - Korean Name Computed Properties
+
+extension Clothes.Category.Top {
+    public var koreanName: String {
+        switch self {
+        case .sweatShirt: return "맨투맨"
+        case .sweater: return "니트"
+        case .hoodie: return "후드티"
+        case .shirtBlouse: return "셔츠/블라우스"
+        case .longSleeve: return "긴팔티"
+        case .sleeveless: return "민소매"
+        case .tShirt: return "반팔티"
+        }
+        
+    }
+    
+}
+
+extension Clothes.Category.Outer {
+    
+    public var koreanName: String {
+        switch self {
+        case .hoodieZipup: return "후드 집업"
+        case .jacket: return "자켓"
+        case .cardigan: return "가디건"
+        case .windbreaker: return "바람막이"
+        case .sportJacket: return "저지"
+        case .thinCoat: return "얇은 코트"
+        case .fieldCoat: return "야상"
+        case .vest: return "조끼"
+        case .shortWinterJacket: return "숏패딩"
+        case .longWinterJacket: return "롱패딩"
+        case .shearlingCoat: return "무스탕"
+        case .fleeceJacket: return "후리스"
+        case .thickCoat: return "두꺼운 코트"
+        case .pufferVest: return "패딩조끼"
+        case .shortCoat: return "숏코트"
+        case .lightweightJacket: return "경량 패딩"
+        }
+    }
+    
+}
+
+extension Clothes.Category.Bottom {
+    
+    public var koreanName: String {
+        switch self {
+        case .longPants: return "긴바지"
+        case .shortPants: return "반바지"
+        case .leggings: return "레깅스"
+        case .overall: return "오버롤"
+        case .miniSkirt: return "미니스커트"
+        case .mediumSkirt: return "미디스커트"
+        case .longSkirt: return "롱스커트"
+        }
+    }
+}
+
+extension Clothes.Category.Dress {
+    public var koreanName: String {
+        switch self {
+        case .miniDress: return "미니 원피스"
+        case .mediumDress: return "미디 원피스"
+        case .longDress: return "롱 원피스"
         }
     }
 }
